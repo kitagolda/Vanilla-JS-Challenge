ae53e2d731765f9d81656012a5833cdadfe23181
Nikita Golda <kitagolda@gmail.com>
2020-10-15 14:22:48 +0300

    4 Vanilla JS Projects Of 10(Notes App added)

---
+++ notes_app/index.js
@@ -0,0 +1,69 @@
+const addBtn = document.querySelector('.add');
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+addBtn.addEventListener("click", () => {
+    addNote();
+});
+
+if(notes){
+    notes.forEach(note => {
+        addNote(note);
+    });
+}
+
+function addNote(text = '') {
+    const note = document.createElement('div');
+    note.classList.add('notes');
+
+    note.innerHTML = `
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main ${text ? '' : 'hidden'}"></div>
+    <textarea class="${text ? 'hidden' : ''}"></textarea>
+    `;
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        note.remove();
+
+        uppdateLS();
+    });
+
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+
+        main.innerHTML = marked(value);
+
+        uppdateLS()
+    });
+
+    document.body.appendChild(note);
+}
+
+function uppdateLS(){
+    const notesText = document.querySelectorAll("textarea");
+    
+    const notes = [];
+
+    notesText.forEach(note => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
